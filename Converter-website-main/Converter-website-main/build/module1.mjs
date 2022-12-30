c6d7b3fb8cec46218a7ac64d5e2e2269252ae4e6
KUNAL KUMAR ROY <89852736+KunalRoy07@users.noreply.github.com>
2022-12-30 19:45:06 +0530

    Add files via upload

---
+++ Converter-website-main/Converter-website-main/build/module1.mjs
@@ -0,0 +1,7 @@
+import D,{b,c,d,e,f} from './module2.mjs'
+console.log(D);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
---
+++ Converter-website-main/Converter-website-main/build/module2.mjs
@@ -0,0 +1,14 @@
+const a="Deepak";
+const b="Prity";
+const c="Payal";
+const d="Manisha";
+const e="Aaradhya";
+const f="Partima";
+
+//export default d;
+export default a;
+export {b};
+export {c};
+export {d};
+export {e};
+export {f};
